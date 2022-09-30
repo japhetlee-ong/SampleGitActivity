275ebb6ed03cdb44cbff3ea9f273fa60927df537
Japhet Ong <japhetlee.dev@gmail.com>
2022-09-30 18:23:19 +0800

    Added MainActivty2.kt

---
+++ app/src/main/java/com/auf/cea/samplegitactivity/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.auf.cea.samplegitactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
